a625e5cca911c9db02667706aca7c3dc8e97a3b9
Rob Crocombe <rjwc1993@yahoo.co.uk>
2018-02-24 23:57:34 +0000

    Develop calendar data

--- src/calendar/calendar.service.js
+++ src/calendar/calendar.service.js
@@ -1,5 +1,39 @@
 import moment from 'moment';
 
 export function getCalendar() {
-  return moment().toString();
+  const now = moment();
+  const startDate = getStartDate(now);
+  const endDate = getEndDate(now);
+  const calendar = [];
+
+  for (let m = startDate.clone(); m.isBefore(endDate); m.add(1, 'days')) {
+    calendar.push(getDayObject(m, now));
+  }
+
+  console.log(calendar);
+
+  return 'hi';
+}
+
+function getStartDate(now) {
+  const startOfMonth = now.clone().startOf('month');
+  return startOfMonth.subtract(startOfMonth.isoWeekday() - 1, 'days');
+}
+
+function getEndDate(now) {
+  const endOfMonth = now.clone().endOf('month');
+  return endOfMonth.add(7 - endOfMonth.isoWeekday(), 'days');
+}
+
+function getDayObject(day, now) {
+  const date = day.clone();
+
+  return {
+    isToday: date.isSame(now, 'day'),
+    isCurrentMonth: date.isSame(now, 'month'),
+    weekDay: date.isoWeekday(),
+    isWeekend: date.isoWeekday() > 5,
+    date,
+    events: {},
+  };
 }
